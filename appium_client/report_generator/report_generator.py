3947b618a30b09af2dcabc661a85201a64c526b3
williamfzc <178894043@qq.com>
2018-01-10 23:14:04 +0800

    output: markdown file

--- appium_client/report_generator/report_generator.py
+++ appium_client/report_generator/report_generator.py
@@ -1,21 +1,21 @@
 import markdown
 
 
-CSS_TEMPLATE = '''
-<style type="text/css">
-h1 {background-color: {COLOR}}
-</style>
-'''
-
-TEMPLATE = '''
+SECTION_TEMPLATE = '''
 # {case_name}
 
-# {status}
+## {status}
 
 ![]({screenshot})
 
 {traceback}
 
+'''
+
+CSS_TEMPLATE = '''
+<style type="text/css">
+h1 {background-color: {COLOR}}
+</style>
 '''
 
 STATUS_DICT = {
@@ -24,28 +24,52 @@ STATUS_DICT = {
 }
 
 
-def markdown2html(report_object, target_path):
-    case_name = report_object.case_name
-    status = 'OK' if report_object.status else 'ERROR'
-    traceback_str = report_object.traceback
-    screen_path = report_object.screenshot
+class ReportGenerator(object):
+    def __init__(self):
+        # markdown 内容
+        self._content = str()
+        self._head = CSS_TEMPLATE.replace('{COLOR}', STATUS_DICT['OK'])
 
-    html_content = CSS_TEMPLATE.replace('{COLOR}', STATUS_DICT[status])
-    html_content += markdown.markdown(
-        TEMPLATE.format(
+    def add_section(self, _report_object):
+        case_name = _report_object.case_name
+        status = 'OK' if _report_object.status else 'ERROR'
+        traceback_str = ('\n' + _report_object.traceback).replace('\n', '\n'+' '*8)
+        screen_path = _report_object.screenshot
+
+        html_content = SECTION_TEMPLATE.format(
             case_name=case_name,
             status=status,
             screenshot=screen_path,
             traceback=traceback_str,
-            css=STATUS_DICT[status]
         )
-    )
 
-    with open(target_path, 'w+') as _html:
-        _html.write(html_content)
+        self._content += html_content
+
+    def build(self):
+        return self._content
+        # return markdown.markdown(self._head + self._content)
 
 
 if __name__ == '__main__':
     from collections import namedtuple
     ReportObject = namedtuple('ReportObject', ['case_name', 'status', 'traceback', 'screenshot'])
-    markdown2html(ReportObject('hello', True, 'aldskfjalkdjbvz,mcxnbvz,mncv', 'nothing'), 'test_for_test.html')
+    report_g = ReportGenerator()
+    report_g.add_section(
+        ReportObject(
+            'case111111', True,
+            'alkdsjhfalkjsdhfaljkfd', 'aa.png'
+        )
+    )
+    report_g.add_section(
+        ReportObject(
+            'case1222222', True,
+            'alkdsjhfal\nkjsdhfaljkfd', 'aa.png'
+        )
+    )
+    report_g.add_section(
+        ReportObject(
+            'case333333331', False,
+            'alkdsjhfal\nkjsdhfaljkfd', 'aa.png'
+        )
+    )
+    print(report_g.build())
